2db50608353f01d573e51a5fe30da341f1a70cd9
JCamp-12 <JChandlerCampbell@gmail.com>
2018-02-23 17:15:06 -0500

    working on hash tables, insert ok, working on collissions

--- src/hash-table.js
+++ src/hash-table.js
@@ -14,13 +14,15 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-    
+    const location = getIndexBelowMax(key, this.limit);
+    this.storage.set(location, value);
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-    
+    const location = getIndexBelowMax(key, this.limit);
+    if (this.storage.get(location)) this.storage.set(location, undefined);
   }
   
   // Fetches the value associated with the given key from the hash table
